dbe329e185060730e3bd38f7551f88c24ab8411a
Harim Hidalgo <77642393+HarimHidal@users.noreply.github.com>
2021-12-13 20:47:51 -0600

    Create AbsMascota.java

---
+++ mascotas/AbsMascota.java
@@ -0,0 +1,33 @@
+// package com.up;
+
+public abstract class AbsMascota implements IntMascota {
+
+    protected String raza;
+    protected int edad;
+    protected String nombre;
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
